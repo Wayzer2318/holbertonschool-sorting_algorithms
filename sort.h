04eef8c8579e6b545992db1857a3b5261de2734a
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-05 19:43:28 +0100

    fixed bug on the sort.h file

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort_recursion(int *array, int low, int high, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 #endif
